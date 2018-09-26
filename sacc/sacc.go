b718f86ee03a505f33392e2ec11548f60de8a162
Sandeep Patel <sandeep.patel@xooa.com>
2018-09-26 12:52:10 +0530

    Commit

--- sacc/sacc.go
+++ sacc/sacc.go
@@ -8,10 +8,10 @@
 
  import (
 	 "fmt"
-	 "os",
-	 "strings",
+	 "os"
+	 "strings"
 	 "github.com/hyperledger/fabric/core/chaincode/shim"
-	 "github.com/hyperledger/fabric/protos/peer",
+	 "github.com/hyperledger/fabric/protos/peer"
 	 "github.com/hyperledger/fabric/core/chaincode/lib/cid"
  )
  
