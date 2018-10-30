b0840a885ca4b07a0829cb354429e3b4214456c9
Sandeep Patel <sandeep.patel@xooa.com>
2018-10-30 09:53:16 +0530

    With Space in path

---
+++ Smart Contract/sacc.go
@@ -0,0 +1,115 @@
+/*
+ * Copyright IBM Corp All Rights Reserved
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ package main
+
+ import (
+	 "fmt"
+	 "os"
+	 "strings"
+	 "github.com/hyperledger/fabric/core/chaincode/shim"
+	 "github.com/hyperledger/fabric/protos/peer"
+	 "github.com/hyperledger/fabric/core/chaincode/lib/cid"
+ )
+ 
+ // SimpleAsset implements a simple chaincode to manage an asset
+ type SimpleAsset struct {
+ }
+ // ...... checking if commit id gets updated
+ // Init is called during chaincode instantiation to initialize any
+ // data. Note that chaincode upgrade also calls this function to reset
+ // or to migrate data.
+ func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	 
+	 return shim.Success(nil)
+ }
+ 
+ // Invoke is called per transaction on the chaincode. Each transaction is
+ // either a 'get' or a 'set' on the asset created by Init function. The Set
+ // method may create a new asset by specifying a new key-value pair.
+ func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	 // Extract the function and args from the transaction proposal
+	 fn, args := stub.GetFunctionAndParameters()
+
+
+
+	//checking for account level access
+	channelId := stub.GetChannelID();
+	accountAssertError := cid.AssertAttributeValue(stub, "ChannelId", channelId)
+	if accountAssertError != nil {
+		return shim.Error(accountAssertError.Error())
+	}
+	
+	// checking for access to app
+	chaincodeId :=os.Getenv("CORE_CHAINCODE_ID_NAME");
+	pair := strings.Split(chaincodeId, ":")
+	chaincodeName := pair[0];
+	appAssertError := cid.AssertAttributeValue(stub, "AppId", chaincodeName)
+	if appAssertError != nil {
+		return shim.Error(appAssertError.Error())
+	}
+
+	
+
+	 fmt.Println("invoke is running " + fn)
+ 
+	 var result string
+	 var err error
+	 if fn == "set" {
+		 result, err = set(stub, args)
+	 } else { // assume 'get' even if fn is nil
+		 result, err = get(stub, args)
+	 }
+	 if err != nil {
+		 return shim.Error(err.Error())
+	 }
+	 fmt.Println("invoke returning " + result)
+	 // Return the result as success payload
+	 return shim.Success([]byte(result))
+ }
+ 
+ // Set stores the asset (both key and value) on the ledger. If the key exists,
+ // it will override the value with the new one
+ func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+	 fmt.Println("- start set value")
+	 if len(args) != 2 {
+		 return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
+	 }
+ 
+	 err := stub.PutState(args[0], []byte(args[1]))
+	 if err != nil {
+		 return "", fmt.Errorf("Failed to set asset: %s", args[0])
+	 }
+	 fmt.Println("- end set value")
+	 return args[1], nil
+ }
+ 
+ // Get returns the value of the specified asset key
+ func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+	 fmt.Println("- start get value")
+	 if len(args) != 1 {
+		 return "", fmt.Errorf("Incorrect arguments. Expecting a key")
+	 }
+ 
+	 
+	 value, err := stub.GetState(args[0])
+	 if err != nil {
+		 return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
+	 }
+	 if value == nil {
+		 return "", fmt.Errorf("Asset not found: %s", args[0])
+	 }
+	 fmt.Println("- end get value")
+	 return string(value), nil
+ }
+ 
+ // main function starts up the chaincode in the container during instantiate
+ func main() {
+	 if err := shim.Start(new(SimpleAsset)); err != nil {
+		 fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+	 }
+ }
+ 
